0ea4a919b23fab0069b8300139fbed2acc0588cf
mong220394 <mong220394@users.noreply.github.com>
2020-02-04 08:33:08 +0800

    Update DBConnection.py

--- DBConnection.py
+++ DBConnection.py
@@ -1,30 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient('localhost',27017).get_database("WazeEmail")
-#client = MongoClient('mongodb://admin:cnat2203@localhost:27017').get_database("Waze")
-#print("teststststts")
-
-
-
-
-        #test = soup.find(name='p', attrs={'style': '3D"font-size:10.5pt;line-height:140%;font-family:"Helveti='})
-        
-       # if test is not None:
-        #    peta = test.find(name='a')
-        #    print("Peta: " + str(peta))
-
-        #peta = soup.find(name='https:')
-"""         peta = soup.find(name='https:')
-        #peta = peta.attrs['https']
-        if peta is not None:
-            peta = 'https://'+ str(peta.find(name='https:').attrs).replace('=\', \'', '').replace('\': \'','').replace('{\'', '').replace('\'}', '').replace('3D','')
-            #print(type(peta))
-            print("Peta: " +peta) """
-
-
-
-#https://apc01.safelinks.protection.outlook.com/?url=https%3A%2F%2Fmandrillapp.com%2Ftrack%2Fclick%2F9825135%2Fwww.waze.com%3Fp%3DeyJzIjoiZ0JNQll0bmF5eHVRakpPM0tPX0tSSG1Gd1dBIiwidiI6MSwicCI6IntcInVcIjo5ODI1MTM1LFwidlwiOjEsXCJ1cmxcIjpcImh0dHBzOlxcXC9cXFwvd3d3LndhemUuY29tXFxcL21zXFxcL2lycmVndWxhcl90cmFmZmljP2Jib3g9MTAxLjIwMyw0LjM5NSwxMDEuMjEzLDQuNDE3JmlkPTE4NjQ4NjcyMyZlbnY9d29ybGRcIixcImlkXCI6XCJmZWMxN2UxZGM1ZDU0ZThmODgzZDNjMDUyYjJmYzJjY1wiLFwidXJsX2lkc1wiOltcIjU1NzlmZDM4YzRkOGZmYTE3N2IwMmNlMDA0NjhlY2VhOTMwZDA1NzFcIl19In0&data=02%7C01%7Cthenghui.chong%40plus.uemnet.com%7Cedaa73cbfbc0414153c708d7a352de8c%7C61ca270ebe514226b35ed6f1d6da30e1%7C0%7C0%7C637157448631466399&sdata=IqNLNZuRHtM6SpOfGCi8Sau1PCKTDSTWV5FaYH1U6yM%3D&reserved=0
-
+client = MongoClient('localhost',27017).get_database("DatabaseName")
 
 
 
